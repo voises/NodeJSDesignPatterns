ec377e0d3f56011cec729f736e8a0318b7bb5375
Moises <47659992+voises@users.noreply.github.com>
2022-09-30 13:42:16 -0700

    init

---
+++ Chapter 01/index.js
@@ -0,0 +1,12 @@
+/**
+ * Notes for chapter 1
+ ***************************** 
+ * Node.js Philosophy
+ * ********
+ * In short, simple is better. Small core, single-purpose modules, and 
+ * above all good practical code
+ * ********
+ * How Node.js works
+ * ********
+ * 
+ */
