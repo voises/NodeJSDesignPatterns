47cbc701a85179f99dfab4ba8ce72ef8133ba8cc
Moises <47659992+voises@users.noreply.github.com>
2022-09-30 14:17:01 -0700

    Added notes for chapter 1

--- Chapter 01/index.js
+++ Chapter 01/index.js
@@ -8,5 +8,11 @@
  * ********
  * How Node.js works
  * ********
- * 
+ * I/O is generally slow during code execution. The problem can be amplified
+ * by how the code is written. Blocking I/O can stall a program, even if it
+ * is multithreaded. 
+ * Non-blocking I/O is better, though even then there 
+ * are better ways of doing it. Using a busy-waiting cycle can be costly,
+ * using the cpu to iterate over an array to poll for resources
+ * Event demultiplexing. 
  */
